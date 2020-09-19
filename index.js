e3a76004d6751ae66197fedbaa544caada908816
Deepak Kumar <64438279+IamIr0nMan@users.noreply.github.com>
2020-09-19 20:15:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-alert("Refresh to roll the dice again");
 var randomNumber1 = (Math.floor(Math.random() * 6)) + 1;
 var randomNumber2 = (Math.floor(Math.random() * 6)) + 1;
 document.querySelector("img.img1").setAttribute("src","images/dice"+randomNumber1+".png");
